c1c9041b34b2872b4d1930e57977a0408c23c48d
Iarlen <iarlensr@hotmail.com>
2023-08-22 00:49:35 -0300

    feat: initial commit.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  import HomeContent from './components/HomeContent.vue';
+  import NftContent from './components/NftContent.vue';
+  
+</script>
+
+<template>
+  <Header />
+  <main class="main">
+    <HomeContent />
+    <NftContent />
+  </main>
+  <Footer />
+</template>
+
+<style lang="sass" scoped>
+  @import "./sass/_variables.sass"
+  
+  .main
+    max-width: 1200px
+    width: 100%
+    min-height: 100vh
+
+    display: flex
+    flex-direction: column
+    gap: $gap-xl
+    padding: 0 $gap-sm
+    padding-bottom: 4rem
+
+    margin: 0 auto
+</style>
---
+++ src/utils/nftCards.ts
@@ -0,0 +1,27 @@
+import nft1 from '../assets/nft-1.png'
+import nft2 from '../assets/nft-2.png'
+import nft3 from '../assets/nft-3.png'
+import nft4 from '../assets/nft-4.png'
+
+export const ntfCards = [
+  {
+    id: 1,
+    image: nft1,
+    background: '#d856d3',
+  },
+  {
+    id: 2,
+    image: nft2,
+    background: '#9268FB',
+  },
+  {
+    id: 3,
+    image: nft3,
+    background: '#29fec7',
+  },
+  {
+    id: 4,
+    image: nft4,
+    background: '#cba1fe',
+  }
+]
