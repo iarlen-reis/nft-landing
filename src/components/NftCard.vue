fe3835d1034b86ad272927c843a703f0a9fc7f7d
Iarlen <iarlensr@hotmail.com>
2023-08-22 00:51:38 -0300

    feat: adding NftCard and NftContent component.

---
+++ src/components/NftCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  defineProps({
+    image: String,
+    background: String,
+  })
+</script>
+
+<template>
+  <div class="nft" :style="{ background: background }">
+    <img :src="image" alt="Imagem do card" v-if="image" />
+  </div>
+</template>
+
+<style scoped lang="sass">
+  @import "../sass/_variables.sass"
+
+  .nft
+    max-width: 400px
+    width: 100%
+    margin: 0 auto
+    
+    border: 6px solid $background-button-black
+    border-radius: $gap-sm
+
+    padding-top: $gap-sm
+
+    img
+      width: 100%
+      height: 100%
+</style>
---
+++ src/components/NftContent.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  import NftCard from './NftCard.vue'
+
+  import  { ntfCards } from '../utils/nftCards'
+
+</script>
+
+<template>
+  <div class="nft__content">
+    <h2>Latest Auction</h2>
+    <div class="nft__cards">
+        <template v-for="card in ntfCards">
+            <NftCard :image="card.image" :background="card.background" />
+        </template>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+  @import "../sass/_variables.sass"
+
+  .nft__content
+    width: 100%
+    display: flex
+    flex-direction: column
+    gap: $gap-base
+
+    margin-top: 3rem
+
+  h2
+    font-family: $font-hanken-grotesk
+    font-size: $text-4xl
+
+  .nft__cards
+    width: 100%
+    display: grid
+    gap: 3rem
+    grid-template-columns: 1fr
+
+    @media (min-width: 640px)
+      grid-template-columns: repeat(2, 1fr)
+
+    @media (min-width: 768px)
+      grid-template-columns: repeat(3, 1fr)
+
+    @media (min-width: 1024px)
+      grid-template-columns: repeat(4, 1fr)
+      justify-content: space-between
+
+</style>
